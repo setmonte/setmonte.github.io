f4f91f3d9dff20ed5a5a3bb06fbfaa829b860297
setmonte <setmonte@gmail.com>
2026-04-24 16:27:21 -0400

    fix: vinho + laranja nas cartas

--- online/tecfe-v7-cards.js
+++ online/tecfe-v7-cards.js
@@ -4,7 +4,7 @@
 var WCSTCards = (function () {
 
   var colorMap = {
-    vermelho: 'red', verde: 'green', amarelo: '#FF8C00', azul: 'blue'
+    vermelho: '#8B0000', verde: 'green', amarelo: '#FF8C00', azul: 'blue'
   };
   var shapeMap = {
     triangulo: 'triangle', estrela: 'star', cruz: 'cross', circulo: 'circle'
